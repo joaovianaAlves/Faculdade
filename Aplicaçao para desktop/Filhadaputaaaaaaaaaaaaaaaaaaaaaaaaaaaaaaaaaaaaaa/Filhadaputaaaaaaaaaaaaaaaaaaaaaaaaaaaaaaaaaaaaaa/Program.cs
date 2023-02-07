4ce6bf1c6396a2c63f694c9d83ab93abf2422c58
João Vitor <62443308+joaovianaAlves@users.noreply.github.com>
2023-02-06 21:12:15 -0300

    Add files via upload

---
+++ Aplicaçao para desktop/Filhadaputaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/Filhadaputaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/Program.cs
@@ -0,0 +1,39 @@
+﻿float altura = 0;
+float b = 0;
+double area;
+
+static float getAltura(float altura){
+    Console.WriteLine("Digite a altura:");
+    altura = float.Parse(Console.ReadLine());
+    {
+        while (altura <= 0)
+        {
+            Console.WriteLine("Valor invalido Redigite:");
+            altura = float.Parse(Console.ReadLine());
+        }
+        return altura;
+}
+}
+
+static float getBase(float b){
+    Console.WriteLine("Digite a base:");
+    b = float.Parse(Console.ReadLine());
+
+    while (b <= 0)
+    {
+        Console.WriteLine("Valor invalido Redigite:");
+        b = float.Parse(Console.ReadLine());
+    }
+    return b;
+}
+
+static void getArea(float altura, float b){
+   
+    float x = getAltura(altura);
+    
+    float y = getBase(b);
+    
+    Console.WriteLine("Area do seu triangulo: " + (y*x) /2);
+}
+
+getArea(altura,b);
