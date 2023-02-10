7f67210ae6447fd1f0bf29eacab506b30e333b44
João Vitor <62443308+joaovianaAlves@users.noreply.github.com>
2023-02-09 21:18:06 -0300

    Add files via upload

---
+++ CalculaVolume/CalculaTemp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcTemp
+{
+    internal class Program
+    {
+           static int CalculaTempFahrenheit(int celsius)
+        {
+            int fahrenheit = (celsius - 160) * 5/9;
+            return fahrenheit;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a temperatura em celsius");
+            int celsius = int.Parse(Console.ReadLine());
+            Console.WriteLine("Temperatura em fahrenheit", celsius , CalculaTempFahrenheit(celsius));
+        }
+    }
+}
