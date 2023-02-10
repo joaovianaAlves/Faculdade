55a2b8a93ae9c51da30884f9ff85708522d43be8
João Vitor <62443308+joaovianaAlves@users.noreply.github.com>
2023-02-09 21:17:21 -0300

    Add files via upload

---
+++ CalculaVolume/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static float CalculaVolume(int r, int a)
+        {
+            float volume = (float)Math.PI * (float)Math.Pow(r, 2) * a;
+            return volume;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o Raio da lata: ");
+            int r = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a altura da lata: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("O volume da lata é´{0:f2}", CalculaVolume(r, a));
+            Console.ReadKey();
+            {
+            }
+        }
+    }
+}
